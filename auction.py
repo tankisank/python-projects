5d4cf35c3747c39a8273db41399f44f5951f84e8
tankisank <77112156+tankisank@users.noreply.github.com>
2022-02-27 14:45:58 +0530

    Create auction.py

---
+++ auction.py
@@ -0,0 +1,31 @@
+logo = '''
+                         ___________
+                         \         /
+                          )_______(
+                          |"""""""|_.-._,.---------.,_.-._
+                          |       | | |               | | ''-.
+                          |       |_| |_             _| |_..-'
+                          |_______| '-' `'---------'` '-'
+                          )"""""""(
+                         /_________\\
+                       .-------------.
+                      /_______________\\
+'''
+def auction():
+    string=input("what is your name")
+    bid=int(input("what is your bid:$"))
+    return string,bid
+
+cond=True
+
+dict={}
+while cond:
+    string,bid=auction()
+    dict[string]=bid
+    bidder=input("another bidder? y or n")
+    if bidder=="y":
+        cond=True
+    else:
+        cond=False
+print(max(dict.values()))        
+    
